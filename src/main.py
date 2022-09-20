426d72ca570a5fb931c13067a934c7817ca938a8
Kartik Soni <kartiksoni2209@gmail.com>
2022-09-20 00:06:26 -0400

    Update main.py

---
+++ src/main.py
@@ -0,0 +1,8 @@
+from cgi import test
+import sys
+from cli import *
+sys.path.append('../../HW/')
+from tests import *
+the = cli_func(sys.argv[1:])
+if the["eg"] != "nothing":
+    runs(the["eg"], the)
