6153d3dc109508f1ade3e7ecb2aa7597b8861785
Tanvi Sinha <tanvi9sinha@gmail.com>
2022-09-20 01:01:11 -0400

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,9 +1,9 @@
-from cgi import test
 import sys
 from cli import *
-sys.path.append('../tests/')
-import test_engine
-# from tests import *
+sys.path.append("../Group3-CSC510-HW2345/")
+# import tests
+from tests import test_engine
 the = cli_func(sys.argv[1:])
 if the["eg"] != "nothing":
     test_engine.runs(the["eg"], the)
+
