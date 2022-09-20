812ad3979933634f858b2a0c260a4cbee26b3536
Tanvi Sinha <tanvi9sinha@gmail.com>
2022-09-20 00:23:42 -0400

    Update main.py

--- src/main.py
+++ src/main.py
@@ -2,7 +2,8 @@ from cgi import test
 import sys
 from cli import *
 sys.path.append('../tests/')
+import test_engine
 # from tests import *
 the = cli_func(sys.argv[1:])
 if the["eg"] != "nothing":
-    tests.test_engine.runs(the["eg"], the)
+    test_engine.runs(the["eg"], the)
